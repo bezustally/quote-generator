360d7a056802d1a1797abc1096ede7d7c97f4852
bezustally <bezustally@gmail.com>
2021-02-27 11:09:43 +0300

    minor fixes

--- script.js
+++ script.js
@@ -22,7 +22,7 @@ async function getQuote() {
     showLoadingSpinner()
     const proxyUrl = "https://serene-inlet-73652.herokuapp.com/"
     const apiUrl = "http://api.forismatic.com/api/1.0/?method=getQuote&lang=ru&format=json";
-    try { 
+    try {
         const response = await fetch(proxyUrl + apiUrl);
         const data = await response.json();
         // If Author is blank, then 'Unknown'
@@ -39,17 +39,17 @@ async function getQuote() {
         }
         quoteText.innerText = data.quoteText;
         removeLoadingSpinner()
-    } catch (error) { 
+    } catch (error) {
     }
 }
 
 // Sharing on VK
 
 function shareVk() {
-     const quote = quoteText.innerText;
-     const author = authorText.innerText;
-     const vkLink = `https://vk.com/share.php?url=http://bezustally.github.io/quote-generator/&title=${quote} © ${author}`;
-     vk.href = vkLink;
+    const quote = quoteText.innerText;
+    const author = authorText.innerText;
+    const vkLink = `https://vk.com/share.php?url=http://bezustally.ru/quote-generator/&title=${quote} © ${author}`;
+    vk.href = vkLink;
 
 }
 
